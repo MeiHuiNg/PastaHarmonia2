b2d26fb5383c9d1af7c02b1aadc30e30781b182c
MeiHuiNg <mei.hui.ng@fusionexgroup.com>
2023-02-21 15:49:14 +0800

    Add Lean tween (Win canvas only)

--- Assets/Script/UITween.cs
+++ Assets/Script/UITween.cs
@@ -12,7 +12,15 @@ public class UITween : MonoBehaviour
     private void Start()
     {
         LeanTween.rotateAround(wheel, Vector3.forward, -360, 10f).setLoopClamp();
-        LeanTween.scale(Amazza, new Vector3(1, 1, 1), 2).setDelay(.5f).setEase(LeanTweenType.easeOutElastic);
-        ;
+        LeanTween.scale(wheel, new Vector3(1, 1, 1), 2).setDelay(0f).setEase(LeanTweenType.easeOutElastic);
+        LeanTween.scale(backPanel, new Vector3(1, 1, 1), 2).setDelay(0f).setEase(LeanTweenType.easeOutElastic);
+        LeanTween.scale(Tomato1, new Vector3(1, 1, 1), 2).setDelay(1f).setEase(LeanTweenType.easeOutElastic);
+        LeanTween.scale(Tomato2, new Vector3(1, 1, 1), 2).setDelay(1.5f).setEase(LeanTweenType.easeOutElastic);
+        LeanTween.scale(Tomato3, new Vector3(1, 1, 1), 2).setDelay(2f).setEase(LeanTweenType.easeOutElastic);
+        LeanTween.scale(Amazza, new Vector3(1, 1, 1), 2).setDelay(3f).setEase(LeanTweenType.easeOutElastic);
+        LeanTween.scale(rabion, new Vector3(1, 1, 1), 2).setDelay(3f).setEase(LeanTweenType.easeOutElastic);
+        LeanTween.scale(NextLevelButton, new Vector3(1, 1, 1), 2).setDelay(4f).setEase(LeanTweenType.easeOutCubic);
+        LeanTween.scale(homeButton, new Vector3(1, 1, 1), 2).setDelay(4f).setEase(LeanTweenType.easeOutCubic);
+        
     }
 }
